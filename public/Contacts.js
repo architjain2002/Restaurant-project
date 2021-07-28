04c4c3e53eb8dc6064752c1472a075c8700d0c0b
Archit Jain <arcyjain2002@gmail.com>
2021-07-28 23:36:49 +0530

    Initial commit

---
+++ public/Contacts.js
@@ -0,0 +1,6 @@
+onsubmition=()=>{
+    document.getElementById("responce").innerHTML="Thankyou for your wonderful feedback.";
+    document.getElementById("contact_form_name").value="";
+    document.getElementById("contact_form_email").value="";
+    document.getElementById("contact_form_feedbacktext").value="";
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const path= require('path');
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"view"));
+app.use( express.static( "public" ) );
+
+app.get('/home',(req,res)=>{
+    res.render("home");
+})
+app.get('/Cuisines',(req,res)=>{
+    res.render("Cuisines");
+})
+app.get('/Special',(req,res)=>{
+    res.render("Special");
+})
+app.get('/Contacts',(req,res)=>{
+    res.render("Contacts");
+})
+
+const hostname='127.0.0.1';
+const port='80';
+
+app.listen(port,hostname,()=>{
+    console.log(`Server running at http://${hostname}:${port}/home`);
+})
